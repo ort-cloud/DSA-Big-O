16748d2b2c2f66e0028d67aa397f501f70a037e0
Ortutay <cortutay@budcotdc.net>
2020-01-30 15:20:50 -0500

    drills added to file

---
+++ drills.js
@@ -0,0 +1,59 @@
+/* 
+Question 1 What is the Big O for this?
+1. O(i) - because it is selecting one object from the array.
+
+2. O(n) - because you are looping through the entire array. */
+
+/* 
+2. Even or odd
+O(i) Only one action is ocurring, one time. No matter what the value of value is. 
+*/
+
+/* 
+3. Are you here?
+O(n^2) nested loops with linear iterations
+*/
+
+/* 
+4. Doubler
+O(n) the function grows linearly based upon teh length of the array.
+*/
+
+/* 
+5. Naive search
+O(n) linear search through the array
+*/
+
+/* 
+6. Creating pairs
+O(n^2) - nested linear operations
+*/
+
+/* 
+7. Compute the sequence
+O(n) - linear loooping through  result array
+
+First iteration 0 is pushed to the end of the array.
+Second 1 is pushed
+and so on.
+*/
+
+/* 
+8. An efficient search
+O(logn) the subset to search gets smaller after each iteration
+*/
+
+/* 
+9. Random element
+O(i)
+*/
+
+/* 
+10. What Am I?
+The function checks to see if the input number is not a prime number. O(n)
+*/
+
+/* 
+11. Tower of Hanoi
+
+*/
